663a0f3fc4649eaead80784c438db2c728940723
czhao1987 <40150843+czhao1987@users.noreply.github.com>
2020-12-21 19:15:49 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -23,42 +23,42 @@ var addressPoints = [
     "Cambridge, UK",
     52.205276,
     0.119167
-  ]
+  ],
   [
     "Southampton, UK",
     50.909698,
     -1.404351
-  ]
+  ],
   [
     "Oxford, UK",
     51.752022,
     -1.257677
-  ]
+  ],
   [
     "Estoril, Portugal",
     38.710159,
     -9.401894
-  ]
+  ],
   [
     "Freiburg, Germany",
     47.997791,
     7.842609
-  ]
+  ],
   [
     "Ilmenau, Germany",
     50.68322,
     10.91858
-  ]
+  ],
   [
     "Besançon, France",
     47.24878,
     6.01815
-  ]
+  ],
   [
     "Berlin, Germany",
     52.520008,
     13.404954
-  ]
+  ],
   [
     "San Francisco, USA",
     37.773972,
