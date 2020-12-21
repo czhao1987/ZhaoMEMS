04154605ce5d6020fd75f015f311f2c9e51db318
czhao1987 <40150843+czhao1987@users.noreply.github.com>
2020-12-21 18:48:33 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,67 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Wuhan, China",
+    30.592850,
+    114.305542
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Leuven, Belgium",
+    50.879829,
+    4.700540
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Xi'an, China",
+    34.341568,
+    108.940178
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Changsha, China",
+    28.228001,
+    112.939003
+  ],
+  [
+    "Cambridge, UK",
+    52.205276,
+    0.119167
+  ]
+  [
+    "Southampton, UK",
+    50.909698,
+    -1.404351
+  ]
+  [
+    "Oxford, UK",
+    51.752022,
+    -1.257677
+  ]
+  [
+    "Estoril, Portugal",
+    38.710159,
+    -9.401894
+  ]
+  [
+    "Freiburg, Germany",
+    47.997791,
+    7.842609
+  ]
+  [
+    "Ilmenau, Germany",
+    50.68322,
+    10.91858
+  ]
+  [
+    "Besançon, France",
+    47.24878,
+    6.01815
+  ]
+  [
+    "Berlin, Germany",
+    52.520008,
+    13.404954
+  ]
+  [
+    "San Francisco, USA",
+    37.773972,
+    -122.431297
   ]
 ];
